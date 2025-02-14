31915b36ac34e56e0fe0ef65a09c5d9dc88eaec0
Abhishek Kumar <rajabhishek7654@gmail.com>
2025-02-14 13:19:09 +0530

    [Abhishek] add. Java Exception Checked Exception Compile-time Exception Solution

---
+++ Java-Exceptions/src/main/java/CheckedException.java
@@ -0,0 +1,30 @@
+import java.io.*;
+
+public class CheckedException {
+    public static void main(String[] args) {
+        String file="exaplefile.txt";
+        try {
+            System.out.println(readFileContents(file));
+        } catch (IOException e) {
+            System.out.println("File not found");
+        }
+    }
+    public static String readFileContents(String filePath) throws FileNotFoundException  {
+        File file = new File(filePath);
+        StringBuilder content = new StringBuilder();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                content.append(line).append("\n");
+            }
+        } catch (FileNotFoundException e) {
+            throw e;
+        } catch (IOException e) {
+            System.out.println("An error occurred while reading the file");
+        }
+
+        return content.toString().trim();
+    }
+
+}
