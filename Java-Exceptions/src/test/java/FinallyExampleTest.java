84c4577199c2a4d2497d74c3285b081ea662f3f2
Abhishek Kumar <rajabhishek7654@gmail.com>
2025-02-14 15:42:12 +0530

    [Abhishek] add. Java Exception finally Block Execution Solution

---
+++ Java-Exceptions/src/test/java/FinallyExampleTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FinallyExampleTest {
+
+    @Test
+    void testValidDivision() {
+        assertEquals(5, FinallyExample.divideNumbers(10, 2));
+    }
+
+    @Test
+    void testDivisionByZero() {
+        assertEquals(0, FinallyExample.divideNumbers(10, 0));
+    }
+}
