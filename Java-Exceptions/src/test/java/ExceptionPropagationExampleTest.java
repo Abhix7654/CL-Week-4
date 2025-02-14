b0f64bae3ad93f15c600097b5b23f338ea64b121
Abhishek Kumar <rajabhishek7654@gmail.com>
2025-02-14 15:54:38 +0530

    [Abhishek] add. Java Exception  Exception Propagation in Methods Solution

---
+++ Java-Exceptions/src/test/java/ExceptionPropagationExampleTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ExceptionPropagationExampleTest {
+
+    @Test
+    void testExceptionPropagation() {
+        Exception exception = assertThrows(ArithmeticException.class, () -> {ExceptionPropagationExample.method1();});
+
+        assertEquals("/ by zero", exception.getMessage());
+    }
+}
