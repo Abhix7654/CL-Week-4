38eec229022a8559d8d15d3dfe89f11fe6e9925e
Abhishek Kumar <rajabhishek7654@gmail.com>
2025-02-15 22:54:35 +0530

    [Abhishek] add. Advanced JUnit Testing Password Strength Validator Solution

---
+++ RegexAndJunit/src/test/java/advancedjunit/PasswordValidatorTest.java
@@ -0,0 +1,23 @@
+package advancedjunit;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class PasswordValidatorTest {
+
+    @Test
+    void testValidPasswords() {
+        assertTrue(PasswordValidator.isValidPassword("Password123"));
+        assertTrue(PasswordValidator.isValidPassword("StrongPass9"));
+
+    }
+
+    @Test
+    void testInvalidPasswords() {
+        assertFalse(PasswordValidator.isValidPassword("pass123"));
+        assertFalse(PasswordValidator.isValidPassword("PASSWORD"));
+
+    }
+    
+}
