e9b24b48a081e4f7c2d871aa830e3413700f1515
Abhishek Kumar <rajabhishek7654@gmail.com>
2025-02-17 19:15:08 +0530

    [Abhihsek] add. Reflection Custom Logging Proxy Using Reflection Solution

---
+++ Annotations_Reflection/src/test/java/reflection/LoggingProxyTest.java
@@ -0,0 +1,21 @@
+package reflection;
+
+
+import org.junit.jupiter.api.Test;
+import reflection.customloggingproxy.Greeting;
+import reflection.customloggingproxy.GreetingImpl;
+import reflection.customloggingproxy.LoggingProxy;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LoggingProxyTest {
+
+    @Test
+    public void testProxyLogging() {
+        Greeting originalGreeting = new GreetingImpl();
+        Greeting proxyGreeting = LoggingProxy.createProxy(originalGreeting, Greeting.class);
+
+        String message = proxyGreeting.sayHello("Abhishek");
+        assertEquals("Hello, Abhishek!", message);
+    }
+}
