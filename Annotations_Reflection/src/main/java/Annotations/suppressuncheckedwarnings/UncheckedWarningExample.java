861763b4e4993f995cb94fc8ecdb487abb89b1c3
Abhishek Kumar <rajabhishek7654@gmail.com>
2025-02-17 21:25:17 +0530

    [Abhihsek] add. Annotations Suppress Unchecked Warnings Solution

---
+++ Annotations_Reflection/src/main/java/Annotations/suppressuncheckedwarnings/UncheckedWarningExample.java
@@ -0,0 +1,17 @@
+package Annotations.suppressuncheckedwarnings;
+import java.util.ArrayList;
+
+public class UncheckedWarningExample {
+
+    @SuppressWarnings("unchecked")
+    public static void main(String[] args) {
+        ArrayList list = new ArrayList();
+
+        list.add("Hello");
+        list.add("World");
+        list.add(100);
+
+        System.out.println("ArrayList: " + list);
+    }
+}
+
