c8cb15390cb77399091faad752c35b730286e46f
Abhishek Kumar <rajabhishek7654@gmail.com>
2025-02-17 21:42:22 +0530

    [Abhihsek] add. Annotations Create a Custom Annotation and Use It Solution

---
+++ Annotations_Reflection/src/main/java/Annotations/customannotation/TaskInfo.java
@@ -0,0 +1,9 @@
+package Annotations.customannotation;
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface TaskInfo {
+    String priority() default "Medium";
+    String assignedTo();
+}
---
+++ Annotations_Reflection/src/main/java/Annotations/customannotation/TaskManager.java
@@ -0,0 +1,17 @@
+package Annotations.customannotation;
+public class TaskManager {
+
+    @TaskInfo(priority = "High", assignedTo = "Abhishek Kumar")
+    public void completeProject() {
+        System.out.println("Completing project");
+    }
+
+    @TaskInfo(assignedTo = "Priya")
+    public void reviewCode() {
+        System.out.println("Reviewing code");
+    }
+
+    public void generalTask() {
+        System.out.println("This task has no annotation");
+    }
+}
